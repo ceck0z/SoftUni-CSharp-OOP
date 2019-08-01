c4f353b8471e9eff951acd355637ae549ec2ccf5
ceck0z <ceck0z@gmail.com>
2019-08-01 23:07:50 +0300

    First Commit

---
+++ Exercises/02. CSharp-OOP-Basics-Working-with-Abstraction/P01_RawData/Cargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_RawData
+{
+    public class Cargo
+    {
+        private int weight;
+
+        public Cargo(int cargoWeight, string cargoType)
+        {
+            this.weight = cargoWeight;
+            this.Type = cargoType;
+        }
+
+        public string Type { get; private set; }
+        
+    }
+}
---
+++ Exercises/02. CSharp-OOP-Basics-Working-with-Abstraction/P01_RawData/Engine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_RawData
+{
+    public class Engine
+    {
+        private int speed;
+
+        public Engine(int engineSpeed, int enginePower)
+        {
+            this.speed = engineSpeed;
+            this.Power = enginePower;
+
+        }
+
+        public int Power { get; private set; }
+    }
+}
